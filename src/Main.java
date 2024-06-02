095159dbe4df7e8f493b394b5518cc574436a9d8
Basilio <daniel.isaes@mail.ru>
2024-06-02 17:08:20 +0300

    Добавлен факультет Пуффендуй

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,16 @@ public class Main {
                 new Gryffindor("Гермиона", "Грейнджер ", 99, 89, 69, 78, 70),
                 new Gryffindor("Рон", "Уизли", 56, 35, 56, 34, 67)
         };
+        Puffenduy[] studentPuffenduy = {new Puffenduy("Захария","Смит",48,78, 45,23,78),
+                new Puffenduy("Седрик","Диггори",67,34, 99,31,52),
+                new Puffenduy("Джастин","Финч-Флетчли",85,49, 58,35,77),
+
+        };
         for (int i = 0; i < studentGriffindor.length; i++) {
             System.out.println(studentGriffindor[i].toString());
+        }
+        for (int i = 0; i < studentPuffenduy.length; i++) {
+            System.out.println(studentPuffenduy[i].toString());
         }
 
     }
---
+++ src/Puffenduy.java
@@ -0,0 +1,49 @@
+public class Puffenduy extends Hogwarts{
+    private int hardworkingStudent;
+    private int loyaltyStudent;
+    private int honestyStudent;
+
+    public Puffenduy(String name, String surname, int powerWitchcraft, int transgressionDistance, int hardworking, int hoyalty, int honesty) {
+        super(name, surname, powerWitchcraft, transgressionDistance);
+        this.hardworkingStudent = hardworking;
+        this.loyaltyStudent = hoyalty;
+        this.honestyStudent = honesty;
+    }
+
+    public int getHardworkingStudent() {
+        return hardworkingStudent;
+    }
+
+    public void setHardworkingStudent(int hardworkingStudent) {
+        this.hardworkingStudent = hardworkingStudent;
+    }
+
+    public int getLoyaltyStudent() {
+        return loyaltyStudent;
+    }
+
+    public void setLoyaltyStudent(int loyaltyStudent) {
+        this.loyaltyStudent = loyaltyStudent;
+    }
+
+    public int getHonestyStudent() {
+        return honestyStudent;
+    }
+
+    public void setHonestyStudent(int honestyStudent) {
+        this.honestyStudent = honestyStudent;
+    }
+
+    @Override
+    public String toString() {
+        return " Пуффендуй:" +
+                " имя = " + getNameStudent() +
+                " фамилия = " + getSurnameStudent() +
+                " мощность колдовства = " + getPowerWitchcraft() +
+                " растояние трансгресии = " + getTransgressionDistance() +
+                " трудолюбие = " + hardworkingStudent +
+                " верность = " + loyaltyStudent +
+                " честность = " + honestyStudent +
+                '}';
+    }
+}
